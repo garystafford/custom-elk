40e755a46653acedc6e37fa56c3d3a1d1777e8f6
Gary A. Stafford <garystafford@rochester.rr.com>
2017-09-20 07:26:31 -0400

    Refactor Dockerfile and config

--- _prelims/docker_commands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Run Docker commands
-
-set -ex
-
-docker build --no-cache -t custom-nginx:1.13.5 .
-docker-compose -f docker-compose.yml -p elk up --force-recreate -d
-docker logs nginx --follow
--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-# disconect from AIP Docker dameon
+# disconect from Docker dameon
 unset DOCKER_TLS_VERIFY DOCKER_CERT_PATH DOCKER_HOST
 
-# delete old image, login to DTR, build new image, and push to DTR
-# source ~/Documents/Notes/dockerhub_login.env
+source ~/Documents/Notes/dockerhub_login.env
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
-docker build --no-cache -t garystafford/custom-elk:latest .
 docker build --no-cache -t garystafford/custom-elk:latest .
 docker push garystafford/hello-logging:latest
