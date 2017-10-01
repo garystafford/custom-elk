2a8f4e77b6781a6b777c818517a73ac565498e45
Gary A. Stafford <garystafford@rochester.rr.com>
2017-09-30 22:32:10 -0400

    Fix build script

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ unset DOCKER_TLS_VERIFY DOCKER_CERT_PATH DOCKER_HOST
 
 source ~/Documents/Notes/dockerhub_login.env
 docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
-docker build --no-cache -t garystafford/custom-elk:latest .
-docker push garystafford/hello-logging:latest
+docker build -t garystafford/custom-elk:561 .
+docker push garystafford/custom-elk:561
